81869ac53bf33adef762b6ecbb3b2a4f3e866b66
Daniela Felipe Soares <daniela.felipe.soares@gmail.com>
2023-11-03 18:17:42 -0300

    implementação de .env.example

--- src/bancodedados/conexao.ts
+++ src/bancodedados/conexao.ts
@@ -4,7 +4,7 @@ export const knex = conexao({
     client: 'pg',
     connection: {
         host: process.env.DB_HOST,
-        port: Number(process.env.DB_PORT),
+        port: Number(process.env.DB_PORT), // passado como number pois o ts reconhece tudo que vem do .env como string, e reclama se não especificar que é number
         user: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_DATABASE
